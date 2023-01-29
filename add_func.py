a467fcba36548d204bd616e470655c1199b97b00
akshin18 <akshinyolcuyev17@gmail.com>
2023-01-29 22:36:22 +0100

    authorization token change

--- add_func.py
+++ add_func.py
@@ -87,7 +87,7 @@ def validate_jwt_token(token):
         # Decode the JWT token and verify its signature
         claims = jwt.decode(token, JWT_SECRET_KEY, algorithms=["HS256"])
         # Check if the user exists in the database
-        user = db.users.find_one({"_id":ObjectId(claims["_id"]) },{"_id":0,"updated_time":0,"created_time":0})
+        user = db.users.find_one({"_id":ObjectId(claims.get("_id","None")) },{"_id":0,"updated_time":0,"created_time":0})
         if user is not None:
             return user
         else:
