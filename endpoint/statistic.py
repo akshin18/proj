3cdcd9a45a67f87fa5fe6fb95e196e6f9b6172d1
akshin18 <akshinyolcuyev17@gmail.com>
2023-02-28 00:53:20 +0100

    update

--- endpoint/statistic.py
+++ endpoint/statistic.py
@@ -14,19 +14,20 @@ from db_func import create_user, add_news, get_news_from_db,delete_news_data
 
 @app.route("/get_statistic",methods=["GET"])
 def get_statistic():
-    data = list(db.channel.find({},{"_id":0}))
-    data["date"] = datetime.now().strftime("%d.%m")
-    all_join = 0
-    all_left = 0
-    all_subscribers = 0
-    for i in data["left_join_stat"]:
-        all_join += i["join"]
-        all_left += i["left"]
-        all_subscribers += i["subscribers"]
-    data["all_join"] = all_join
-    data["all_left"] = all_left
-    data["all_subscribers"] = all_subscribers
-    response = jsonify({"status":1,"data":data})
+    datas = list(db.channel.find({},{"_id":0}))
+    for data in datas:
+        data["date"] = datetime.now().strftime("%d.%m")
+        all_join = 0
+        all_left = 0
+        all_subscribers = 0
+        for i in data["left_join_stat"]:
+            all_join += i["join"]
+            all_left += i["left"]
+            all_subscribers += i["subscribers"]
+        data["all_join"] = all_join
+        data["all_left"] = all_left
+        data["all_subscribers"] = all_subscribers
+    response = jsonify({"status":1,"data":datas})
     return response
 
 
