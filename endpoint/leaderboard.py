85c67ef46108d577ee720a0aa37952e7a8b7ee8f
akshin18 <akshinyolcuyev17@gmail.com>
2023-03-11 01:04:25 +0100

    update

---
+++ endpoint/leaderboard.py
@@ -0,0 +1,26 @@
+from app import app
+from flask import request,jsonify
+from add_func import check_register_entity
+from flask_cors import cross_origin
+from bson import json_util
+import json
+from datetime import datetime
+
+from config import db, PROJ_STATE
+from add_func import generate_jwt_token, validate_jwt_token
+from db_func import create_user, add_news, get_news_from_db,delete_news_data
+
+
+
+@app.route("/get_leaderboard",methods=["GET"])
+def get_statistic():
+    key = request.args.get("key",None)
+    if not key :
+        key = "dep"
+    datas = list(db.channel.find({},{"_id":0}).sort( { "key": -1 } ))
+    for data in datas:
+        data["date"] = datetime.now().strftime("%d.%m")
+        data["percen"] = round(((data["sub_count"]/data["left_join_stat"][0]["subscribers"])-1)*100,2)
+    response = jsonify({"status":1,"data":datas})
+    return response
+
